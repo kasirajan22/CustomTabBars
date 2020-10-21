67a8201d16bd73ce54c58d9497d1b91eef78b5b6
kasirajan22 <kasi.srm@gmail.com>
2020-10-21 06:22:46 +0530

    Initial Commit

---
+++ CustomTabBars/CustomTabBarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarsApp.swift
+//  CustomTabBars
+//
+//  Created by Magizhan on 21/10/20.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
